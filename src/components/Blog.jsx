a5ff4f161111dd43016c530ba8af1cc8d5cd7a74
Burben <burbenburbenov@gmail.com>
2025-12-17 12:50:04 +0200

    Update Blog.jsx

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div className="blog">
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog, handleLike }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid 1px',
+    marginBottom: 5
+  }
+
+  return (
+    <div className="blog" style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={() => setVisible(!visible)}>
+        {visible ? 'hide' : 'view'}
+      </button>
+
+      {visible && (
+        <div className="blogDetails">
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes}
+            <button onClick={handleLike}>like</button>
+          </div>
+          <div>{blog.user && blog.user.name}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
