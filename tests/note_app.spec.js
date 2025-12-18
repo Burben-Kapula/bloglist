946ea2885c6cde50154fbb4b638e383e5ddd1e02
Burben <burbenburbenov@gmail.com>
2025-12-18 11:04:14 +0200

    18 грудня

---
+++ tests/note_app.spec.js
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test'
+
+test('front page can be opened', async ({ page }) => {
+  await page.goto('http://localhost:5173')
+  await expect(page.getByText('Notes')).toBeVisible()
+})
